190f94b504c06dde77c8c4dba35b58acd371925f
Moataz Soliman <motaz8413@gmail.com>
2026-03-01 02:06:03 +0200

    Pgvector upgrade

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -5,6 +5,13 @@ from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from models import ProcessingEnum
+from typing import List
+from dataclasses import dataclass
+
+@dataclass
+class Document:
+    page_content: str
+    metadata: dict
 
 class ProcessController(BaseController):
 
@@ -63,10 +70,44 @@ class ProcessController(BaseController):
             for rec in file_content
         ]
 
-        chunks = text_splitter.create_documents(
-            file_content_texts,
-            metadatas=file_content_metadata
+        # chunks = text_splitter.create_documents(
+        #     file_content_texts,
+        #     metadatas=file_content_metadata
+        # )
+
+        chunks = self.process_simpler_splitter(
+            texts=file_content_texts,
+            metadatas=file_content_metadata,
+            chunk_size=chunk_size,
         )
+
+        return chunks
+
+    def process_simpler_splitter(self, texts: List[str], metadatas: List[dict], chunk_size: int, splitter_tag: str="\n"):
+        
+        full_text = " ".join(texts)
+
+        # split by splitter_tag
+        lines = [ doc.strip() for doc in full_text.split(splitter_tag) if len(doc.strip()) > 1 ]
+
+        chunks = []
+        current_chunk = ""
+
+        for line in lines:
+            current_chunk += line + splitter_tag
+            if len(current_chunk) >= chunk_size:
+                chunks.append(Document(
+                    page_content=current_chunk.strip(),
+                    metadata={}
+                ))
+
+                current_chunk = ""
+
+        if len(current_chunk) >= 0:
+            chunks.append(Document(
+                page_content=current_chunk.strip(),
+                metadata={}
+            ))
 
         return chunks
 
